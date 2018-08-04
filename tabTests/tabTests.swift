20b069db1982676cc4232b620ef08e915cb653a4
402-07 <a402-07@402-07ui-imac.local>
2018-08-04 12:23:13 +0900

    Initial Commit

---
+++ tabTests/tabTests.swift
@@ -0,0 +1,36 @@
+//
+//  tabTests.swift
+//  tabTests
+//
+//  Created by 402-07 on 2018. 8. 4..
+//  Copyright © 2018년 moonbc. All rights reserved.
+//
+
+import XCTest
+@testable import tab
+
+class tabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
